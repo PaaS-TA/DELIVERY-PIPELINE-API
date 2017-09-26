e0accf4ce33df7cb170e0d06c3f37904070b5314
REX <rexx4314@gmail.com>
2017-09-26 15:26:21 +0900

    modify comment

--- src/main/java/paasta/delivery/pipeline/api/job/JobController.java
+++ src/main/java/paasta/delivery/pipeline/api/job/JobController.java
@@ -137,6 +137,7 @@ class JobController {
      */
     @RequestMapping(value = "/rearrange-job-order", method = RequestMethod.POST)
     public CustomJob rearrangeJobOrder(@RequestBody CustomJob customJob) {
+        // TODO
         return jobService.rearrangeJobOrder(customJob);
     }
 
